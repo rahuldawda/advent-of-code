07537fc29b908b8a4432c470dd17b7adcdab36ac
Rahul Dawda <rahul.dawda@hexagon.com>
2021-11-30 21:34:56 +0530

    Create Day1.py
    Initial Commit

---
+++ Day1.py
@@ -0,0 +1,20 @@
+from collections import *
+from functools import lru_cache
+import fileinput
+import heapq
+import itertools
+import math
+import random
+import re
+import sys
+
+def part1(input):
+    return -1
+
+def part2(input):
+    return -1
+
+data = open("day1.txt", "r").read().splitlines()
+
+print(part1(data))
+print(part2(data))
